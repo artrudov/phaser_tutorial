80a5fbb03f7c98fd504cc02a3fdb4cb01d6efb7b
ArtRudov <artrudov@gmail.com>
2020-04-22 01:25:41 +0300

    fix code style in webpack config

--- scripts/base.js
+++ scripts/base.js
@@ -1,60 +1,60 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+        use: 'raw-loader'
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
+        use: 'file-loader'
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      app: path.resolve(__dirname, "src/"),
-      common: path.resolve(__dirname, "src/common/"),
-      const: path.resolve(__dirname, "src/const/"),
-      utils: path.resolve(__dirname, "src/utils/"),
-      types: path.resolve(__dirname, "src/types/"),
-      assets: path.resolve(__dirname, "public/assets/")
+      app: path.resolve(__dirname, 'src/'),
+      common: path.resolve(__dirname, 'src/common/'),
+      const: path.resolve(__dirname, 'src/const/'),
+      utils: path.resolve(__dirname, 'src/utils/'),
+      types: path.resolve(__dirname, 'src/types/'),
+      assets: path.resolve(__dirname, 'public/assets/')
     }
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../dist')
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, '../')
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: './public/index.html'
     })
   ]
-};
+}
